9067e685f584a1e33fd95819840e30843b37dc96
joeyPark8 <61180930+joeyPark8@users.noreply.github.com>
2020-12-05 23:32:55 +0900

    Add files via upload

---
+++ PositionPoint/src/hotspot/Main.java
@@ -0,0 +1,108 @@
+package hotspot;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main extends JavaPlugin implements Listener {
+    ArrayList<Location> spot = new ArrayList<Location>();
+
+    @Override
+    public void onEnable() {
+        System.out.println("Hotspot is activated");
+    }
+
+    @Override
+    public void onDisable() {
+        System.out.println("Hotspot is deactivated");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        Player player = (Player) sender;
+        if (command.getName().equalsIgnoreCase("/spot")) {
+            if (args.length == 0) {
+                sender.sendMessage(ChatColor.LIGHT_PURPLE + "Hotspot developed by Semin");
+            }
+            else if (args[0].equalsIgnoreCase("add")) {
+                spot.add(player.getLocation());
+                sender.sendMessage(ChatColor.GREEN + "add spot at " + player.getLocation());
+            }
+            else if (args[0].equalsIgnoreCase("remove")) {
+                if (!(args[1].isEmpty())) {
+                    if (isInt(args[1])) {
+                        int num = Integer.parseInt(args[0]);
+                        if (num > 0 && num <= spot.size()) {
+                            spot.remove(num);
+                        }
+                        else {
+                            sender.sendMessage(ChatColor.RED + "please write integer argument from 1 to " + spot.size());
+                        }
+                    }
+                    else {
+                        sender.sendMessage(ChatColor.RED + "please write integer argument");
+                    }
+                }
+                else {
+                    sender.sendMessage(ChatColor.RED + "please write integer argument");
+                }
+            }
+            else if (args[0].equalsIgnoreCase("getlist")) {
+                ItemStack stack = new ItemStack(Material.PAPER);
+                ItemMeta meta = stack.getItemMeta();
+
+                meta.setDisplayName("spot list");
+                meta.setLore(Arrays.asList("list for see all spot"));
+                stack.setItemMeta(meta);
+
+                player.getInventory().addItem(stack);
+            }
+        }
+        else if (command.getName().equalsIgnoreCase("/go")) {
+            if (args.length > 0) {
+                sender.sendMessage(ChatColor.RED + "please write integer argument");
+            }
+            else {
+                if (isInt(args[0])) {
+                    int num = Integer.parseInt(args[0]);
+                    player.teleport(spot.get(num - 1));
+                    sender.sendMessage(ChatColor.GOLD + "thwip!");
+                }
+                else {
+                    sender.sendMessage(ChatColor.RED + "please write integer argument");
+                }
+            }
+        }
+        return true;
+    }
+
+    boolean isInt(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    @EventHandler
+    public void playerInteract(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+        if (player.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("spot list")) {
+
+        }
+    }
+}
