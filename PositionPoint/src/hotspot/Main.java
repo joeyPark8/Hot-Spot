0eb3e8ff2cf5fe98f78f1c6c05a3a15b82e74a1b
joeyPark8 <61180930+joeyPark8@users.noreply.github.com>
2020-12-06 20:39:20 +0900

    Add files via upload

--- PositionPoint/src/hotspot/Main.java
+++ PositionPoint/src/hotspot/Main.java
@@ -6,6 +6,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -17,16 +18,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
-public class Main extends JavaPlugin implements Listener {
+public class Main extends JavaPlugin implements Listener, TabCompleter {
     ArrayList<Location> spot = new ArrayList<Location>();
 
     @Override
     public void onEnable() {
         System.out.println("Hotspot is activated");
         Bukkit.getPluginManager().registerEvents(this, this);
+        getCommand("/spot").setExecutor(this::onCommand);
+        getCommand("/spot").setTabCompleter(this::onTabComplete);
+        getCommand("/go").setExecutor(this::onCommand);
     }
-    
+
     @Override
     public void onDisable() {
         System.out.println("Hotspot is deactivated");
@@ -99,6 +104,21 @@ public class Main extends JavaPlugin implements Listener {
         catch (NumberFormatException e) {
             return false;
         }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("/spot")) {
+            if (args.length == 1) {
+                List<String> arguments = new ArrayList<>();
+                arguments.add("add");
+                arguments.add("remove");
+                arguments.add("getlist");
+
+                return arguments;
+            }
+        }
+        return null;
     }
 
     @EventHandler
